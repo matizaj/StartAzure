d838cf7a07a1e8f550373f922793437ffad5af6d
Mateusz Zajac <TRBC37@motorolasolutions.com>
2020-02-04 10:19:27 +0100

    Add project files.

---
+++ StartAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StartAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
